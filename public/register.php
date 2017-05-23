04e1b8aad1d8709e6e3fcdd768fb6f31048a4e12
Marco Pivetta <ocramius@gmail.com>
2017-05-23 10:43:43 +0200

    Example registration with a filesystem-based storage

--- public/register.php
+++ public/register.php
@@ -1,11 +1,16 @@
 <?php
 
-// registering a new user:
+use Authentication\Entity\User;
+use Authentication\Infrastructure\Repository\FilesystemUsers;
 
-// 1. check if a user with the same email address exists
-// 2. if not, create a user
-// 3. hash the password
-// 4. send the email to confirm activation (we will just display it)
-// 5. save the user
+require_once __DIR__ . '/../vendor/autoload.php';
 
-// Tip: discuss - email or saving? Chicken-egg problem
+$users = new FilesystemUsers(__DIR__ . '/../data/users');
+
+$users->add(User::register(
+    $_POST['emailAddress'],
+    $_POST['password'],
+    function (string $password) : string {
+        return password_hash($password, \PASSWORD_DEFAULT);
+    }
+));
--- src/Authentication/Entity/User.php
+++ src/Authentication/Entity/User.php
@@ -4,5 +4,39 @@ namespace Authentication\Entity;
 
 class User
 {
-    
+    /**
+     * @var string
+     */
+    private $email;
+
+    /**
+     * @var string
+     */
+    private $passwordHash;
+
+    private function __construct(string $email, string $passwordHash)
+    {
+        $this->email = $email;
+        $this->passwordHash = $passwordHash;
+    }
+
+    public static function register(
+        string $email,
+        string $password,
+        callable $hashingMechanism
+    ) : self {
+        if (! filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            throw new \InvalidArgumentException(sprintf(
+                'Invalid email "%s" provided',
+                $email
+            ));
+        }
+
+        return new self($email, $hashingMechanism($password));
+    }
+
+    public function id() : string
+    {
+        return $this->email;
+    }
 }
